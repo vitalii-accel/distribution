aad6d49974c8ec308d9d5116f27148ca58208d16
Vitalii Chenchovyi <orgvitaliy@gmail.com>
2023-04-15 17:39:32 +0300

    check env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,32 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { config } from './config';
 import { DistributionModule } from './distribution/distribution.module';
 
+console.log();
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => {
-        return {
-          type: 'mysql',
-          host: configService.get('mysqlHost'),
-          port: configService.get('mysqlPort'),
-          username: configService.get('mysqlUsername'),
-          password: configService.get('mysqlPassword'),
-          database: configService.get('mysqlDatabase'),
-          entities: [],
-          autoLoadEntities: true,
-          synchronize: true,
-        };
-      },
-      inject: [ConfigService],
-    }),
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    // useFactory: (configService: ConfigService) => {
+    //   return {
+    //     type: 'mysql',
+    //     host: configService.get('mysqlHost'),
+    //     port: configService.get('mysqlPort'),
+    //     username: configService.get('mysqlUsername'),
+    //     password: configService.get('mysqlPassword'),
+    //     database: configService.get('mysqlDatabase'),
+    //     entities: [],
+    //     autoLoadEntities: true,
+    //     synchronize: true,
+    //   };
+    // },
+    //   inject: [ConfigService],
+    // }),
     AccountsModule,
     AuthModule,
     DistributionModule,
@@ -35,4 +36,12 @@ import { DistributionModule } from './distribution/distribution.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) {
+    console.log(configService.get('mysqlHost'));
+    console.log(configService.get('mysqlPort'));
+    console.log(configService.get('mysqlUsername'));
+    console.log(configService.get('mysqlPassword'));
+    console.log(configService.get('mysqlDatabase'));
+  }
+}

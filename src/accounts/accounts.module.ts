f6658bf5f5a71beabfd7b2e80a53e5caf8353d20
Vitalii Chenchovyi <orgvitaliy@gmail.com>
2023-04-15 18:39:03 +0300

    check env

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -1,14 +1,14 @@
-import { forwardRef, Module } from '@nestjs/common';
-import { AccountsService } from './accounts.service';
-// import { Account } from './account.entity';
-import { AuthModule } from '../auth/auth.module';
-
-@Module({
-  imports: [
-    forwardRef(() => AuthModule),
-    // TypeOrmModule.forFeature([Account])
-  ],
-  providers: [AccountsService],
-  exports: [AccountsService],
-})
-export class AccountsModule {}
+// import { forwardRef, Module } from '@nestjs/common';
+// import { AccountsService } from './accounts.service';
+// // import { Account } from './account.entity';
+// import { AuthModule } from '../auth/auth.module';
+//
+// @Module({
+//   imports: [
+//     forwardRef(() => AuthModule),
+//     // TypeOrmModule.forFeature([Account])
+//   ],
+//   providers: [AccountsService],
+//   exports: [AccountsService],
+// })
+// export class AccountsModule {}
--- src/distribution/distribution.module.ts
+++ src/distribution/distribution.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
-import { DistributionService } from './distribution.service';
-import { DistributionController } from './distribution.controller';
-import { AccountsModule } from '../accounts/accounts.module';
-
-@Module({
-  imports: [AccountsModule],
-  providers: [DistributionService],
-  controllers: [DistributionController],
-})
-export class DistributionModule {}
+// import { Module } from '@nestjs/common';
+// import { DistributionService } from './distribution.service';
+// import { DistributionController } from './distribution.controller';
+// import { AccountsModule } from '../accounts/accounts.module';
+//
+// @Module({
+//   imports: [AccountsModule],
+//   providers: [DistributionService],
+//   controllers: [DistributionController],
+// })
+// export class DistributionModule {}

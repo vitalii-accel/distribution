576e24319f85aff698807db1621be7961c4d241b
Vitalii Chenchovyi <orgvitaliy@gmail.com>
2023-04-15 17:57:34 +0300

    check env

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -1,6 +1,5 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 // import { Account } from './account.entity';
 import { AuthModule } from '../auth/auth.module';
 
